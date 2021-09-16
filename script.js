ecb7351e78b901fcd63a9a95f4212fb84838a5a1
Hernan Di Tano <hditano@gmail.com>
2021-09-16 09:03:15 -0300

    added basic text

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mainCard = document.querySelector('.main-card');
 let myArray = [];
+let myObject = {};
 
 fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
         "method": "GET",
@@ -10,8 +11,9 @@ fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
     })
     .then(response => response.json())
     .then(data => {
-        myArray = data.slice(0,21);
-        for(let array of myArray) {
+        myArray = data.slice(0, 21);
+
+        for (let array of myArray) {
             console.log(array.thumbnail);
         }
         renderData()
@@ -19,7 +21,11 @@ fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
 
 
 function renderData() {
-    for(let array of myArray) {
-        mainCard.innerHTML += `<div class="card"><img class="cards-thumbnail" src="${array.thumbnail}"><a id="text-hover">Prueba Prueba</a></div>`
+    for (let i = 0; i < myArray.length; i++) {
+        mainCard.innerHTML += ` <div class="card">
+                                <img class="cards-thumbnail" src="${myArray[i].thumbnail}">
+                                <a id="text-hover">${myArray[i].title}</a>
+                                <a id="text-content">${myArray[i].genre} ${myArray[i].publisher}</a>
+                                </div>`
     }
-}  
+}
